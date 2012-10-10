f33fabdf44a9c894e189e91d9cea871b4df1dce3
Claude Falguiere <cfalguiere@gmail.com>
2012-10-10 22:22:50 +0200

    removed unused libs

--- perf-project-backup/project.clj
+++ perf-project-backup/project.clj
@@ -1,9 +1,7 @@
 (defproject perf-project "1.0.0-SNAPSHOT"
   :description "demos incanter in lein"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [incanter "1.3.0"]
-		 [org.clojure/algo.generic "0.1.0"]
-		 [clj-time "0.4.4"]]
+		 [incanter "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.4.2"][lein-midje "1.0.10"][com.stuartsierra/lazytest "1.2.3"]]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :main perf-project.core)
